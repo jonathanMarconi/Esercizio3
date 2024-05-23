269da965e71353137ae41ed48839b140c7b61d2a
Jonathan Rigatti <116878991+jonathanMarconi@users.noreply.github.com>
2024-05-23 19:37:12 +0000

    finish

---
+++ libri.js
@@ -0,0 +1,21 @@
+const libri = [
+    {
+        "id": "1",
+        "titolo": "Harry Potta",
+        "autore": "J.K Roling",
+        "annoPubblicazione": "1998"
+    },
+    {
+        "id":"2",
+        "titolo":"guida galattica agli autostoppisti",
+        "autore": "Douglas Adams",
+        "annoPubblicazione": "1997"
+    },
+    {
+        "id":"3",
+        "titolo": "il monaco che vendette la ferrari",
+        "autore": "Robin S.sharm",
+        "annoPubblicazione": "1996"
+    }
+]
+module.exports = {libri};
